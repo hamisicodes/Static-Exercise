fb72455b50b4eb55e46166d1f3276e2d743f88b1
Hamisi Ngao <newhamisi@gmail.com>
2023-05-04 14:22:21 -0400

    complete static exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(60);
+            var farenheit = TempConverter.CelsiusToFahrenheit(30);
+
+            Console.WriteLine($"60 farenheit to celsius is {celsius} celsius");
+            Console.WriteLine($"30 celsius to farenheit is {farenheit} farenheit");
         }
     }
 }
